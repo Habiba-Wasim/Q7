6ed3d8e80f47729418548c9e869e75b982c3b23b
Habiba-Wasim <160347457+Habiba-Wasim@users.noreply.github.com>
2024-02-23 20:58:52 +0500

    Step7 Of Typescript
    Step7 Of Typescript

---
+++ Q7.js
@@ -0,0 +1,18 @@
+"use strict";
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//1Addition
+let a = 10;
+let b = 5;
+console.log(a + b);
+//2sSubtraction
+let c = 10;
+let d = 5;
+console.log(c - d);
+//3Multiplication
+let e = 10;
+let f = 5;
+console.log(e * f);
+//4Division
+let g = 10;
+let h = 5;
+console.log(g / h);
---
+++ Q7.ts
@@ -0,0 +1,25 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//1Addition
+
+let a: number = 10
+let b: number = 5
+
+console.log(a + b);
+
+//2sSubtraction
+let c: number = 10
+let d: number = 5
+
+console.log(c - d);
+
+//3Multiplication
+let e: number = 10
+let f: number = 5
+
+console.log(e * f);
+
+//4Division
+let g: number = 10
+let h: number = 5
+
+console.log(g / h);
